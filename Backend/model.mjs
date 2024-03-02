f486d3a4c8086b47267fe593d97d5db837830aa4
Carson274 <carsonsecrest@gmail.com>
2024-03-02 01:38:50 -0800

    Implemented MongoDB connection and CRUD operations for movies using Mongoose

---
+++ Backend/model.mjs
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+// connect to to the database
+const db = mongoose.connection;
+
+// the open event is called when the database connection successfully opens
+db.once('open', () => {
+  console.log('Successfully connected to MongoDB using Mongoose!');
+});
+
+// define the schema
+const movieSchema = new mongoose.Schema({
+  id: { type: Number, required: true },
+  budget: { type: Number, required: true },
+  popularity: { type: Number, required: true },
+  release_date: { type: String, required: true },
+  revenue: { type: Number, required: true },
+  title: { type: String, required: true },
+  poster_url: { type: String, required: true }
+});
+
+// compile the model from the schema. This must be done after defining the schema
+const Movie = mongoose.model("Movie", movieSchema)
+
+const createMovie = async (id, budget, popularity, release_date, revenue, title, poster_url) => {
+    const movie = new Movie({ id: id, budget: budget, popularity: popularity, release_date: release_date, revenue: revenue, title: title, poster_url: poster_url});
+    return movie.save();
+}
+
+const findMovieById = async (filter) => {
+    const query = await Movie.findOne(filter);
+    return query;
+}
+
+const findMovies = async () => {
+    const query = await Movie.find();
+    return query;
+}
+
+const updateMovie = async (filter, update) => {
+    const result = await Movie.updateOne(filter, update);
+    return result.modifiedCount;
+};
+
+const deleteMovie = async (filter) => {
+    const result = await Movie.deleteOne(filter);
+    return result.deletedCount;
+}
+
+export { createMovie, findMovieById, findMovies, updateMovie, deleteMovie };
