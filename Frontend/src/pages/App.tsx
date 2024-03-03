ba6648f22ec007295f3b91cc7b14628c2aa88570
SeanRK04 <seanrkhanna@gmail.com>
2024-03-02 20:27:35 -0800

    Added transitions to home page

--- Frontend/src/pages/App.tsx
+++ Frontend/src/pages/App.tsx
@@ -1,58 +1,61 @@
-import './styles/index.css'
-import './styles/global.css'
-import './styles/leaderboard.css'
-import './styles/app.css'
+import '../styles/index.css';
+import '../styles/global.css';
+import '../styles/leaderboard.css';
+import '../styles/App.css';
+import { redirect } from 'react-router-dom';
 
 const App = () => {
+
+  const handleButtonClick = () => {
+    console.log('Button clicked')
+    const title = document.querySelector('#title') as HTMLHeadingElement ;
+    title.style.transform = 'translateX(-1000px)'
+
+    const leaderboard = document.querySelector('.leaderboard-wrapper') as HTMLDivElement;
+    leaderboard.style.transform = 'translateX(1000px)'
+
+    const playButton = document.querySelector('.play-button-wrapper') as HTMLDivElement;
+    playButton.style.transform = 'translateX(-1000px)'
+
+    setTimeout(() => {
+      console.log('Redirecting to /play');
+      redirect('/play');
+    }, 3000);
+  };
+
   return (
     <>
-    <h1 className="megapunch-font red-gradient">BOX OFFICE <br /> <span className="blue-font">BATTLES</span></h1>  
-      <button className="play-button large-font italic-text">ENTER THE RING</button>
+      <h1 id="title" className="megapunch-font red-gradient">BOX OFFICE <br /> <span className="blue-font">BATTLES</span></h1>  
+      <div className="play-button-wrapper" onClick={handleButtonClick}>
+          <button onClick={handleButtonClick} className="play-button large-font italic-text">ENTER THE RING</button>        
+      </div>
       <div className="leaderboard-wrapper">
-        <div className="leaderboard-container">
-          <h3 className="leaderboard-title">Leaderboards</h3>
-          <table className="centered-table">
-            <thead>
-              <tr>
-                <th className="italic-text">Player</th>
-                <th className="italic-text">Streak</th>
+        <h3 className="leaderboard-title">Leaderboards</h3>
+        <table className="centered-table">
+          <thead>
+            <tr>
+              <th className="italic-text">Player</th>
+              <th className="italic-text">Streak</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <tr key={index}>
+                <td className="small-font">Player {index + 1}</td>
+                <td>{index + 1}</td>
               </tr>
-            </thead>
-            <tbody>
-              {Array.from({ length: 10 }).map((_, index) => (
-                <tr key={index}>
-                  <td className="small-font">Player {index + 1}</td>
-                  <td>{index + 1}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <div className="image-wrapper">
-        <div className="image-container">
-          <img className="shifted-up" src="/Images/Ring.svg" height="600px" width="1000px" alt="Boxing Ring" />
-        </div>
-      </div>
+      <img className="ring shifted-up" src="../../public/Images/Ring.svg" height="600px" width="1000px" alt="Boxing Ring" />
     </>
   )
 }
 
 export default App;
 
-import { Link } from 'react-router-dom'
-import '../styles/App.css'
-import '../styles/global.css'
 
-function App() {
-  
-  return (
-    <div id='app-container'>
-      <h1 className="megapunch-font red-gradient">Box Office Battles</h1>
-      <Link to="/play" className="normal-font play-button">Play Now</Link>
-    </div>
-  )
-}
 
 // TODO: Below code is for future mouse glow effect on the logo
 
@@ -81,10 +84,4 @@ function App() {
 //       };
 //     }, []);
 //     return mousePosition;
-<<<<<<< HEAD:Frontend/src/index.tsx
 //   };
-=======
-//   };
-
-export default App
->>>>>>> 7160c556c028a3daf55265a5531be4d5c7966984:Frontend/src/pages/App.tsx
--- Frontend/src/pages/Play.tsx
+++ Frontend/src/pages/Play.tsx
@@ -1,6 +1,6 @@
-import '../styles/Play.css'
-import ring from '../../public/ring.svg'
-import { useEffect, useState } from 'react'
+import '../styles/Play.css';
+import ring from '../../public/Images/Ring.svg';
+import { useEffect, useState } from 'react';
 
 interface Movie {
     _id: string;
@@ -14,48 +14,52 @@ interface Movie {
 }
 
 const Play = () => {
-  const [firstMovie, setFirstMovie] = useState<Movie | null>(null);
-  const [secondMovie, setSecondMovie] = useState<Movie | null>(null);
+    const [firstMovie, setFirstMovie] = useState<Movie | null>(null);
+    const [secondMovie, setSecondMovie] = useState<Movie | null>(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    (async () => {
-        const firstMovie: Movie = await fetch('http://3.15.198.16:3000/getOriginalMovie/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        })
-        .then(response => response.json());
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response1 = await fetch('http://3.15.198.16:3000/getOriginalMovie/');
+                const firstMovieData: Movie = await response1.json();
 
-        const secondMovie: Movie = await fetch('http://3.15.198.16:3000/getNewMovie/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({usedMovies: [firstMovie]})
-            })
-            .then(response => response.json());
+                const response2 = await fetch('http://3.15.198.16:3000/getNewMovie/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ usedMovies: [firstMovieData] }),
+                });
+                const secondMovieData: Movie = await response2.json();
 
-            console.log(firstMovie, secondMovie)
-        setFirstMovie(firstMovie);
-        setSecondMovie(secondMovie);
-      })();
-  }, []);
+                setFirstMovie(firstMovieData);
+                setSecondMovie(secondMovieData);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching movie data:', error);
+                setLoading(false);
+            }
+        };
 
-  return (
-    <div id='play-container'>
-      <div id='movie-title-container'>
-        <h2 className="movie-title">{firstMovie?.title}</h2>
-        <h2 id='vs'>V.S.</h2>
-        <h2 className="movie-title">{secondMovie?.title}</h2>
-      </div>
-      <div id='ring-container'>
-        <img src={ring} alt="ring" id="ring" />
-        <img src={firstMovie?.poster_url} alt="movie1" id="movie1" />
-        <img src={secondMovie?.poster_url} alt="movie2" id="movie2" />
-      </div>
-    </div>
-  )
-}
+        fetchMovies();
+    }, []);
 
-export default Play
+    return (
+        <div id='play-container'>
+            <div id='movie-title-container'>
+                <h2 className="movie-title">{loading ? 'Loading...' : firstMovie?.title}</h2>
+                <h2 id='vs'>V.S.</h2>
+                <h2 className="movie-title">{loading ? 'Loading...' : secondMovie?.title}</h2>
+            </div>
+            <div id='ring-container'>
+                <img src={ring} alt="ring" id="ring" />
+                <img src={loading ? '' : firstMovie?.poster_url} alt="movie1" id="movie1" />
+                <img src={loading ? '' : secondMovie?.poster_url} alt="movie2" id="movie2" />
+                    {loading ? 'Loading...' : 'ENTER THE RING'}
+            </div>
+        </div>
+    );
+};
+
+export default Play;
