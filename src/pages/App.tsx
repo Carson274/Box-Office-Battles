ade5656739603f626a9454c09a53e60f8b45e388
Sean Khanna <seankhanna@Seans-MacBook-Air.local>
2024-03-02 20:14:18 -0800

    Added mainpage transition

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,45 +1,61 @@
-import '../styles/index.css'
-import '../styles/global.css'
-import '../styles/leaderboard.css'
-import '../styles/App.css'
-import { Link } from 'react-router-dom'
+import '../styles/index.css';
+import '../styles/global.css';
+import '../styles/leaderboard.css';
+import '../styles/App.css';
+import { redirect } from 'react-router-dom';
 
 const App = () => {
+
+  const handleButtonClick = () => {
+    console.log('Button clicked')
+    const title = document.querySelector('#title') as HTMLHeadingElement ;
+    title.style.transform = 'translateX(-1000px)'
+
+    const leaderboard = document.querySelector('.leaderboard-wrapper') as HTMLDivElement;
+    leaderboard.style.transform = 'translateX(1000px)'
+
+    const playButton = document.querySelector('.play-button-wrapper') as HTMLDivElement;
+    playButton.style.transform = 'translateX(-1000px)'
+
+    setTimeout(() => {
+      console.log('Redirecting to /play');
+      redirect('/play');
+    }, 3000);
+  };
+
   return (
     <>
-    <h1 className="megapunch-font red-gradient">BOX OFFICE <br /> <span className="blue-font">BATTLES</span></h1>  
-      <Link to="/play "className="play-button large-font italic-text">ENTER THE RING</Link>
+      <h1 id="title" className="megapunch-font red-gradient">BOX OFFICE <br /> <span className="blue-font">BATTLES</span></h1>  
+      <div className="play-button-wrapper" onClick={handleButtonClick}>
+          <button onClick={handleButtonClick} className="play-button large-font italic-text">ENTER THE RING</button>        
+      </div>
       <div className="leaderboard-wrapper">
-        <div className="leaderboard-container">
-          <h3 className="leaderboard-title">Leaderboards</h3>
-          <table className="centered-table">
-            <thead>
-              <tr>
-                <th className="italic-text">Player</th>
-                <th className="italic-text">Streak</th>
+        <h3 className="leaderboard-title">Leaderboards</h3>
+        <table className="centered-table">
+          <thead>
+            <tr>
+              <th className="italic-text">Player</th>
+              <th className="italic-text">Streak</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 10 }).map((_, index) => (
+              <tr key={index}>
+                <td className="small-font">Player {index + 1}</td>
+                <td>{index + 1}</td>
               </tr>
-            </thead>
-            <tbody>
-              {Array.from({ length: 10 }).map((_, index) => (
-                <tr key={index}>
-                  <td className="small-font">Player {index + 1}</td>
-                  <td>{index + 1}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <div className="image-wrapper">
-        <div className="image-container">
-          <img className="shifted-up" src="/Images/Ring.svg" height="600px" width="1000px" alt="Boxing Ring" />
-        </div>
-      </div>
+      <img className="ring shifted-up" src="../../public/Images/Ring.svg" height="600px" width="1000px" alt="Boxing Ring" />
     </>
   )
 }
 
 export default App;
+
+
 
 // TODO: Below code is for future mouse glow effect on the logo
 
