6d3429c13046cbab0f4aa83ac17ed008f42856bb
Seba <sebastiantorresola3@gmail.com>
2024-03-03 00:40:46 -0800

    Completed punch and fall animations to movie component logic

--- Frontend/src/components/MovieComponent.tsx
+++ Frontend/src/components/MovieComponent.tsx
@@ -2,31 +2,38 @@ import { useEffect } from 'react';
 import '../styles/MovieComponent.css';
 import MovieComponentProps from '../types/MovieComponentPropsType';
 
-const MovieComponent = ({ setHasLoaded, hasLoaded, lastUpdated, movieType, movie, handleMovieClick }) => {
+const MovieComponent = ({ winner, movieType, movie, handleMovieClick }: MovieComponentProps) => {
+  useEffect(() => {
+    const leftMovie = document.querySelector<HTMLImageElement>('.left-movie')!;
+    const rightMovie = document.querySelector<HTMLImageElement>('.right-movie')!;
+    const leftGlove = document.querySelector<HTMLImageElement>('.left-glove')!;
+    const rightGlove = document.querySelector<HTMLImageElement>('.right-glove')!;
+    // animate both in
+    if (movieType === 'left-movie') {
+      leftMovie.style.animation = 'slide-left 2s ease-out';
+      leftGlove.style.animation = 'slide-left 2s ease-out';
+    } else {
+        rightGlove.style.animation = 'slide-right 2s ease-out';
+        rightMovie.style.animation = 'slide-right 2s ease-out';
+    }
+    
+  }, []);
+
+  // // New movie gets entered
   useEffect(() => {
     const leftMovie = document.querySelector<HTMLImageElement>('.left-movie')!;
     const rightMovie = document.querySelector<HTMLImageElement>('.right-movie')!;
     const leftGlove = document.querySelector<HTMLImageElement>('.left-glove')!;
     const rightGlove = document.querySelector<HTMLImageElement>('.right-glove')!;
 
-    // check if this is the first time the component is rendered
-    if(!hasLoaded) {
-      setHasLoaded(true);
-
-      // animate both in
-      leftMovie.style.animation = 'slide-left 1.5s ease-out';
-      rightMovie.style.animation = 'slide-right 1.5s ease-out';
-      leftGlove.style.animation = 'slide-left 1.5s ease-out';
-      rightGlove.style.animation = 'slide-right 1.5s ease-out';
+    if (leftMovie.alt === movie.title) {
+      leftMovie.style.animation = 'slide-left 2s ease-out';
+      leftGlove.style.animation = 'slide-left 2s ease-out';
     }
 
-    if (leftMovie && leftGlove && lastUpdated === 'first-movie') {
-      leftMovie.style.animation = 'slide-left 1.5s ease-out';
-      leftGlove.style.animation = 'slide-left 1.5s ease-out';
-    }
-    if (rightMovie && rightGlove && lastUpdated === 'second-movie') {
-      rightMovie.style.animation = 'slide-right 1.5s ease-out';
-      rightGlove.style.animation = 'slide-right 1.5s ease-out';
+    if (rightMovie.alt === movie.title) {
+      rightMovie.style.animation = 'slide-right 2s ease-out';
+      rightGlove.style.animation = 'slide-right 2s ease-out';
     }
 
     // remove the animation after it finishes
@@ -35,8 +42,36 @@ const MovieComponent = ({ setHasLoaded, hasLoaded, lastUpdated, movieType, movie
       leftGlove.style.animation = '';
       rightMovie.style.animation = '';
       rightGlove.style.animation = '';
-    }, 1500);
-  });
+    }, 2000);
+  }, [movie]);
+
+  useEffect(() => {
+    if (!winner) return;
+    const leftMovie = document.querySelector<HTMLImageElement>('.left-movie')!;
+    const rightMovie = document.querySelector<HTMLImageElement>('.right-movie')!;
+    const leftGlove = document.querySelector<HTMLImageElement>('.left-glove')!;
+    const rightGlove = document.querySelector<HTMLImageElement>('.right-glove')!;
+
+    if (leftMovie && leftMovie.alt !== winner.title) {
+      // do falling animation
+      console.log("left movie falling")
+      leftMovie.style.animation = 'fall 2s ease-out';
+      leftGlove.style.animation = 'fall 2s ease-out';
+    } else {
+      // do punching animation
+      console.log("left movie punching")
+      leftGlove.style.animation = 'punch-right 0.43s ease-out';
+    }
+
+    if (rightMovie && rightMovie.alt !== winner.title) {
+      // do falling animation, right movie lost
+      rightMovie.style.animation = 'fall 2s ease-out';
+      rightGlove.style.animation = 'fall 2s ease-out';
+    } else {
+      // do punching animation, right movie won
+      rightGlove.style.animation = 'punch-left 0.43s ease-out';
+    }
+  }, [winner]);
 
   if(movieType === 'left-movie') {
     return (
